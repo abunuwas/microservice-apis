461f6ed824e0b7f537eff7844fb74a0a33b5f484
abunuwas <joseharoperalta@gmail.com>
2020-08-26 14:43:24 +0100

    ch02: Add orders application layer and hook it up with the API layer

--- ch02/orders/api/api.py
+++ ch02/orders/api/api.py
@@ -1,58 +1,74 @@
 from typing import List
 from uuid import UUID
 
+from fastapi import HTTPException
 from starlette import status
 
 from orders.app import app
+from orders.orders.orders import OrdersCollection, Order
 from orders.api.schemas import GetOrderSchema, CreateOrderSchema
 
-
-order = {
-    'id': 'ff0f1355-e821-4178-9567-550dec27a373',
-    'status': 'completed',
-    'created': 1740493805,
-    'order': [
-        {
-            'product': 'cappuccino',
-            'size': 'medium',
-            'quantity': 1
-        }
-    ]
-}
+orders_collection = OrdersCollection()
 
 
 @app.get('/orders', response_model=List[GetOrderSchema])
 def get_orders():
     return [
-        order
+        order.dict() for order in orders_collection.to_list()
     ]
 
 
 @app.post('/orders', status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
 def create_order(order_details: CreateOrderSchema):
-    return order
+    order = Order(order_details.order, orders_collection).create()
+    return order.dict()
 
 
 @app.get('/orders/{order_id}', response_model=GetOrderSchema)
 def get_order(order_id: UUID):
+    order = orders_collection.get(order_id)
+    if order is None:
+        raise HTTPException(
+            status_code=404, detail=f'Order with ID {order_id} not found'
+        )
     return order
 
 
 @app.put('/orders/{order_id}', response_model=GetOrderSchema)
 def update_order(order_id: UUID, order_details: CreateOrderSchema):
-    return order
+    order = orders_collection.get(order_id)
+    if order is None:
+        raise HTTPException(
+            status_code=404, detail=f'Order with ID {order_id} not found'
+        )
+    return order.update(order_details).dict()
 
 
 @app.delete('/orders/{order_id}', response_model=GetOrderSchema)
 def delete_order(order_id: UUID):
-    return
+    order = orders_collection.get(order_id)
+    if order is None:
+        raise HTTPException(
+            status_code=404, detail=f'Order with ID {order_id} not found'
+        )
+    order.delete()
 
 
 @app.post('/orders/{order_id}/cancel', response_model=GetOrderSchema)
 def cancel_order(order_id: UUID):
-    return order
+    order = orders_collection.get(order_id)
+    if order is None:
+        raise HTTPException(
+            status_code=404, detail=f'Order with ID {order_id} not found'
+        )
+    return order.cancel().dict()
 
 
 @app.post('/orders/{order_id}/pay', response_model=GetOrderSchema)
 def pay_order(order_id: UUID):
-    return order
+    order = orders_collection.get(order_id)
+    if order is None:
+        raise HTTPException(
+            status_code=404, detail=f'Order with ID {order_id} not found'
+        )
+    return order.pay().dict()
---
+++ ch02/orders/orders/orders.py
@@ -0,0 +1,94 @@
+import time
+import uuid
+from dataclasses import dataclass
+from typing import Dict, List
+
+from orders.api.schemas import OrderItemSchema
+
+
+class OrdersCollection:
+
+    def __init__(self):
+        self._collection = []
+
+    def __iter__(self):
+        for order in self._collection:
+            yield order
+
+    def add(self, order):
+        self._collection.append(order)
+        return len(self._collection) - 1
+
+    def to_list(self):
+        return self._collection
+
+    def get(self, order_id):
+        for order in self._collection:
+            if order.id == order_id:
+                return order
+
+    def delete(self, index):
+        self._collection.pop(index)
+
+
+@dataclass
+class OrderItem:
+    order_item: OrderItemSchema
+
+    def __post_init__(self):
+        self.product = self.order_item.product
+        self.size = self.order_item.size
+        self.quantity = self.order_item.quantity
+
+    def dict(self):
+        return self.__dict__
+
+
+class Order:
+
+    def __init__(self, order_items: List[OrderItemSchema], collection: OrdersCollection):
+        self.order = [
+            OrderItem(order_item) for order_item in order_items
+        ]
+        self._collection = collection
+
+        self.id = None
+        self.status = None
+        self.created = None
+        self.updated = None
+        self._collection_index = None
+
+    def create(self):
+        self.id = uuid.uuid4()
+        self.status = 'active'
+        self.created = time.time()
+        self.updated = time.time()
+        self._collection_index = self._collection.add(self)
+        return self
+
+    def update(self, order_items: List[Dict]):
+        self.order = [
+            OrderItem(**order_item) for order_item in order_items
+        ]
+        self.updated = time.time()
+        return self
+
+    def delete(self):
+        self._collection.delete(self._collection_index)
+
+    def pay(self):
+        self.status = 'completed'
+        return self
+
+    def cancel(self):
+        self.status = 'cancelled'
+        return self
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'status': self.status,
+            'created': self.created,
+            'updated': self.updated,
+            'order': [order.dict() for order in self.order]
+        }
