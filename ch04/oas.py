5bf1dfaa7485c94f7dd5f2a63fb3e1d156a2c13a
abunuwas <joseharoperalta@gmail.com>
2020-08-26 16:33:38 +0100

    ch05: Add API specification endpoints

--- ch04/oas.py
+++ ch04/oas.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+from unittest import TestCase
+
+import yaml
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
 from marshmallow import Schema, fields, validate
+
+from oas_helpers import make_response, make_request_body, make_parameter
 
 specification = APISpec(
     title='Orders API',
@@ -51,3 +57,55 @@ class GetOrderSchema(Schema):
 specification.components.schema('OrderItemSchema', schema=OrderItemSchema)
 specification.components.schema('GetOrderSchema', schema=GetOrderSchema)
 specification.components.schema('CreateOrderSchema', schema=CreateOrderSchema)
+
+
+specification.path(
+    path='/orders',
+    operations={
+        'get': {
+            'description': (
+                'A list of orders made by the customer sorted by date. Allows '
+                'to filter orders by range of dates.\n'
+            ),
+            'responses': make_response(
+                {'type': 'object', 'properties': {'data': {'type': 'array', 'items': {'$ref': '#/components/schemas/GetOrderSchema'}}}},
+                description='A JSON array of orders'
+            )
+        },
+        'post': {
+            'summary': 'Creates an Order',
+            'requestBody': make_request_body('CreateOrderSchema'),
+            'responses': make_response(
+                'GetOrderSchema', status_code='201',
+                description='A JSON representation of the created order'
+            )
+        }
+    }
+)
+
+
+specification.path(
+    path='/orders/{order_id}',
+    parameters=[make_parameter(in_='path', name='order_id', schema={'type': 'string'})],
+    operations={
+        'get': {
+            'summary': 'Returns the details of a specific order',
+            'responses': make_response(
+                'GetOrderSchema', description='A JSON representation of an order'
+            )
+        },
+        'put': {
+            'description': 'Replaces an existing order',
+            'requestBody': make_request_body('CreateOrderSchema'),
+            'responses': make_response(
+                'GetOrderSchema', description='A JSON representation of an order'
+            )
+        },
+        'delete': {
+            'description': 'Deletes an existing order',
+            'responses': {'204': {'description': 'The resource was deleted successfully'}}
+        }
+    }
+)
+
+print(specification.to_yaml())
