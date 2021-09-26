d9ada82fe4946b92f8384ee589507ad894fe41b3
abunuwas <joseharoperalta@gmail.com>
2021-09-26 23:50:51 +0100

    Add orders API implementation

---
+++ ch12/orders/orders/api/schemas.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field, Extra, conint
+
+
+class Size(str, Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class Status(str, Enum):
+    created = 'created'
+    paid = 'paid'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1)] = 1
+
+    class Config:
+        extra = Extra.forbid
+
+
+class CreateOrderSchema(BaseModel):
+    order: List[OrderItemSchema]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class GetOrderSchema(CreateOrderSchema):
+    id: UUID
+    created: datetime
+    status: Status
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]
+
+    class Config:
+        extra = Extra.forbid
