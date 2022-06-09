d20fc85a0639507adf7c79d7819d241cd13bfd4c
abunuwas <joseharoperalta@gmail.com>
2022-06-09 17:49:55 +0100

    Serve orders API design in ch06

--- ch06/orders/orders/app.py
+++ ch06/orders/orders/app.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
+import yaml
 from fastapi import FastAPI
 
-app = FastAPI(debug=True)
+app = FastAPI(debug=True, openapi_url="/openapi/orders.json", docs_url="/docs/orders")
 
+oas_doc = yaml.safe_load((Path(__file__).parent / "../oas.yaml").read_text())
+
+app.openapi = lambda: oas_doc
 
 from orders.api import api
